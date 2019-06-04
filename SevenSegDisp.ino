1b90ab0b13333a79a5185635383865d8ffc9f8d0
falcuun <falcuun@gmail.com>
2019-06-04 21:51:40 +0200

    Added segment pins defins and pin modes

--- SevenSegDisp.ino
+++ SevenSegDisp.ino
@@ -1,2 +1,24 @@
-void setup(){}
-void loop(){}
+#define FIRST 2
+#define SECOND 3
+#define THIRD 4
+#define FOURTH 5
+#define FIFTH 6
+#define SIXTH 7
+#define SEVENTH 8
+
+
+void setup()
+{
+  pinMode(FIRST, OUTPUT);
+  pinMode(SECOND, OUTPUT);
+  pinMode(THIRD, OUTPUT);
+  pinMode(FOURTH, OUTPUT);
+  pinMode(FIFTH, OUTPUT);
+  pinMode(SIXTH, OUTPUT);
+  pinMode(SEVENTH, OUTPUT);
+
+  Serial.begin(9600);
+}
+
+void loop()
+{}
