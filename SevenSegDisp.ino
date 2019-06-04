30d096be6e06414d8cfb0b0a3af8f44e82cd807a
falcuun <falcuun@gmail.com>
2019-06-05 01:29:50 +0200

    Added a separate function for each number and make a simple count down function

--- SevenSegDisp.ino
+++ SevenSegDisp.ino
@@ -22,15 +22,99 @@ void setup()
 
 void loop()
 {
-  digitalWrite(FIRST, LOW);
-  digitalWrite(SECOND, LOW);
-  digitalWrite(THIRD, LOW);
-  digitalWrite(FOURTH, LOW);
-  digitalWrite(FIFTH, LOW);
-  digitalWrite(SIXTH, LOW);
-  digitalWrite(SEVENTH, LOW);
-  delay(1000);
+  counting();
+}
 
+void counting()
+{
+  OFF();
+  one();
+  delay(1000);
+  OFF();
+  two();
+  delay(1000);
+  OFF();
+  three();
+  delay(1000);
+  OFF();
+  four();
+  delay(1000);
+  OFF();
+  five();
+  delay(1000);
+  OFF();
+  six();
+  delay(1000);
+  OFF();
+  seven();
+  delay(1000);
+  OFF();
+  eight();
+  delay(1000);
+  OFF();
+  nine();
+  delay(1000);
+  OFF();
+  zero();
+  delay(1000);
+}
+
+void one()
+{
+  digitalWrite(SECOND, HIGH);
+  digitalWrite(THIRD, HIGH);
+}
+
+void two()
+{
+  digitalWrite(FIRST, HIGH);
+  digitalWrite(SIXTH, HIGH);
+  digitalWrite(SEVENTH, HIGH);
+  digitalWrite(THIRD, HIGH);
+  digitalWrite(FOURTH, HIGH);
+}
+
+void three()
+{
+  digitalWrite(FIRST, HIGH);
+  digitalWrite(SECOND, HIGH);
+  digitalWrite(THIRD, HIGH);
+  digitalWrite(FOURTH, HIGH);
+  digitalWrite(SEVENTH, HIGH);
+}
+
+void four()
+{
+  digitalWrite(FIFTH, HIGH);
+  digitalWrite(SEVENTH, HIGH);
+  digitalWrite(THIRD, HIGH);
+  digitalWrite(SECOND, HIGH);
+}
+void five()
+{
+  digitalWrite(FIRST, HIGH);
+  digitalWrite(SECOND, HIGH);
+  digitalWrite(SEVENTH, HIGH);
+  digitalWrite(FIFTH, HIGH);
+  digitalWrite(FOURTH, HIGH);
+}
+void six()
+{
+  digitalWrite(FIFTH, HIGH);
+  digitalWrite(FOURTH, HIGH);
+  digitalWrite(SEVENTH, HIGH);
+  digitalWrite(FIRST, HIGH);
+  digitalWrite(SECOND, HIGH);
+  digitalWrite(SIXTH, HIGH);
+}
+void seven()
+{
+  digitalWrite(FOURTH, HIGH);
+  digitalWrite(SECOND, HIGH);
+  digitalWrite(THIRD, HIGH);
+}
+void eight()
+{
   digitalWrite(FIRST, HIGH);
   digitalWrite(SECOND, HIGH);
   digitalWrite(THIRD, HIGH);
@@ -38,6 +122,34 @@ void loop()
   digitalWrite(FIFTH, HIGH);
   digitalWrite(SIXTH, HIGH);
   digitalWrite(SEVENTH, HIGH);
+}
+void nine()
+{
+  digitalWrite(FIRST, HIGH);
+  digitalWrite(SECOND, HIGH);
+  digitalWrite(THIRD, HIGH);
+  digitalWrite(FOURTH, HIGH);
+  digitalWrite(FIFTH, HIGH);
+  digitalWrite(SEVENTH, HIGH);
+}
 
-  delay(1000);
+void zero()
+{
+  digitalWrite(FIRST, HIGH);
+  digitalWrite(SECOND, HIGH);
+  digitalWrite(THIRD, HIGH);
+  digitalWrite(FOURTH, HIGH);
+  digitalWrite(FIFTH, HIGH);
+  digitalWrite(SIXTH, HIGH);
+}
+
+void OFF()
+{
+  digitalWrite(FIRST, LOW);
+  digitalWrite(SECOND, LOW);
+  digitalWrite(THIRD, LOW);
+  digitalWrite(FOURTH, LOW);
+  digitalWrite(FIFTH, LOW);
+  digitalWrite(SIXTH, LOW);
+  digitalWrite(SEVENTH, LOW);
 }
